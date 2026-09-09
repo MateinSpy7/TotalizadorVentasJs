e6c791d56d72e598b845d25a5ba92614e5df4316
Mateo Quintanilla <quintanillacalvimateo@gmail.com>
2026-09-08 23:57:18 -0400

    feat: Ver total con impuesto para TX y NV

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,5 +1,11 @@
-function totalizador(precioUnitario, cantidad) {
-  return precioUnitario * cantidad;
+function totalizador(precioUnitario, cantidad, estado = "") {
+  const neto = precioUnitario * cantidad;
+  const tasasImpuesto = { UT: 0.0665, NV: 0.08, TX: 0.0625, AL: 0.04, CA: 0.0825 };
+  
+  const tasa = tasasImpuesto[estado] || 0;
+  const impuesto = neto * tasa;
+  
+  return neto + impuesto;
 }
 
 export default totalizador;
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -4,4 +4,17 @@ describe("Totalizador", () => {
   it("deberia calcular el precio neto", () => {
     expect(totalizador(3, 2)).toEqual(6);
   });
+
+  it("deberia calcular el precio neto", () => {
+    expect(totalizador(20, 15)).toEqual(300);
+  });
+
+  it("deberia calcular el precio total con el impuesto aplicado para TX (6.25%)", () => {
+    expect(totalizador(20, 3, "TX")).toEqual(63.75);
+  });
+
+  it("deberia calcular el precio total con el impuesto aplicado para NV (8%)", () => {
+    expect(totalizador(10, 3, "NV")).toEqual(32.4);
+  });
+
 });
