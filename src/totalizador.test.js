eb1e8c47936f4f4ce8436113a56abf8dde397e7e
Mateo Quintanilla <quintanillacalvimateo@gmail.com>
2026-09-09 00:04:12 -0400

    feat: total con impuesto para todos los estados

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -13,8 +13,21 @@ describe("Totalizador", () => {
     expect(totalizador(20, 3, "TX")).toEqual(63.75);
   });
 
-  it("deberia calcular el precio total con el impuesto aplicado para NV (8%)", () => {
+  it("deberia calcular el precio total con el impuesto aplicado para UT (6.65%)", () => {
+    expect(totalizador(10, 2, "UT")).toEqual(21.33);
+  });
+
+  it("deberia calcular el precio total con el impuesto aplicado para AL (4%)", () => {
+    expect(totalizador(15, 4, "AL")).toEqual(62.4);
+  });
+
+  it("deberia calcular el precio total con el impuesto aplicado para CA (8.25%)", () => {
+    expect(totalizador(25, 2, "CA")).toEqual(54.125);
+  });
+
+   it("deberia calcular el precio total con el impuesto aplicado para NV (8%)", () => {
     expect(totalizador(10, 3, "NV")).toEqual(32.4);
   });
+
 
 });
